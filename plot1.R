7428fb845b64b5bd93d63d925356c9e94fa9448f
ldberriz <ldberriz@ctsx.net>
2015-01-06 17:00:37 -0500

    First commit

---
+++ plot1.R
@@ -0,0 +1,26 @@
+fh <- file("household_power_consumption.txt")
+dta <- read.table(
+      text = grep("^[1,2]/2/2007", readLines(fh), value = TRUE), 
+      col.names = c(
+                  "Date", 
+                  "Time", 
+                  "Global_active_power", 
+                  "Global_reactive_power", 
+                  "Voltage", 
+                  "Global_intensity", 
+                  "Sub_metering_1",
+                  "Sub_metering_2", 
+                  "Sub_metering_3"), 
+      sep = ";", 
+      header = TRUE, 
+      na.strings = "?")
+close(fh)
+summary(dta)
+
+png(filename = "./plot1.png",width = 480,height = 480)
+hist(dta$Global_active_power, 
+     col = "red", 
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)")
+dev.off()
+
